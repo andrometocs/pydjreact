d036bb8d3473621c71b276f800c7904359bfc88a
andrometocs <kumaran.kanniappan@andrometocs.com>
2021-01-04 00:31:52 +0530

    first commit

--- pydjreact/settings.py
+++ pydjreact/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'j*dau*7w=r=227g_+2_(k83x9b=z=qz%iozzcvj&!p0@y78=-j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["https://sampleflaskreact.herokuapp.com/"]
 
 # Application definition
 
